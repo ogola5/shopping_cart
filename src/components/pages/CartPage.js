a6852529722decf8108dca9556e5c093fe7c3faa
ogola5 <ogolaevance5@.com>
2023-12-11 19:44:52 +0300

    added other checkpout,cart and homepage

---
+++ src/components/pages/CartPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function CartPage({ cartItems, removeFromCart, adjustQuantity }) {
+  return (
+    <div className="cart-page">
+      <h1>Your Shopping Cart</h1>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <div>
+          {cartItems.map(item => (
+            <div key={item.id} className="cart-item">
+              <h2>{item.name}</h2>
+              <p>Price: ${item.price}</p>
+              <p>Quantity: <input type="number" value={item.quantity} onChange={(e) => adjustQuantity(item.id, e.target.value)} /></p>
+              <button onClick={() => removeFromCart(item.id)}>Remove</button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
--- src/components/pages/ProductDetailPage.js
+++ src/components/pages/ProductDetailPage.js
@@ -1,9 +1,89 @@
 import React, { useState } from 'react';
 
 export default function ProductDetailPage() {
-  // Sample array of products
+  // Sample array of products with unique IDs and dummy image paths
   const products = [
     // Add 10 products here with id, name, description, price, imageUrl, availability
+    {
+      id: 1,
+      name: 'Example Product',
+      description: 'This is a detailed description of the Example Product.',
+      price: 29.99,
+      imageUrl: 'path_to_dummy_image', // replace with your image path
+      availability: 'In Stock'
+    },
+    {
+      id: 1,
+      name: 'Example Product',
+      description: 'This is a detailed description of the Example Product.',
+      price: 29.99,
+      imageUrl: 'path_to_dummy_image', // replace with your image path
+      availability: 'In Stock'
+    },
+    {
+      id: 1,
+      name: 'Example Product',
+      description: 'This is a detailed description of the Example Product.',
+      price: 29.99,
+      imageUrl: 'path_to_dummy_image', // replace with your image path
+      availability: 'In Stock'
+    },
+    {
+      id: 1,
+      name: 'Example Product',
+      description: 'This is a detailed description of the Example Product.',
+      price: 29.99,
+      imageUrl: 'path_to_dummy_image', // replace with your image path
+      availability: 'In Stock'
+    },
+    {
+      id: 1,
+      name: 'Example Product',
+      description: 'This is a detailed description of the Example Product.',
+      price: 29.99,
+      imageUrl: 'path_to_dummy_image', // replace with your image path
+      availability: 'In Stock'
+    },
+    {
+      id: 1,
+      name: 'Example Product',
+      description: 'This is a detailed description of the Example Product.',
+      price: 29.99,
+      imageUrl: 'path_to_dummy_image', // replace with your image path
+      availability: 'In Stock'
+    },
+    {
+      id: 1,
+      name: 'Example Product',
+      description: 'This is a detailed description of the Example Product.',
+      price: 29.99,
+      imageUrl: 'path_to_dummy_image', // replace with your image path
+      availability: 'In Stock'
+    },
+    {
+      id: 1,
+      name: 'Example Product',
+      description: 'This is a detailed description of the Example Product.',
+      price: 29.99,
+      imageUrl: 'path_to_dummy_image', // replace with your image path
+      availability: 'In Stock'
+    },
+    {
+      id: 1,
+      name: 'Example Product',
+      description: 'This is a detailed description of the Example Product.',
+      price: 29.99,
+      imageUrl: 'path_to_dummy_image', // replace with your image path
+      availability: 'In Stock'
+    },
+    {
+      id: 1,
+      name: 'Example Product',
+      description: 'This is a detailed description of the Example Product.',
+      price: 29.99,
+      imageUrl: 'path_to_dummy_image', // replace with your image path
+      availability: 'In Stock'
+    }
   ];
 
   // State for tracking cart count
@@ -15,15 +95,15 @@ export default function ProductDetailPage() {
   };
 
   return (
-    <div>
+    <div className="product-detail-page">
       <h1>Our Products</h1>
-      <div>
+      <div className="product-list">
         {products.map(product => (
-          <div key={product.id}>
+          <div key={product.id} className="product">
             <h2>{product.name}</h2>
-            <img src={product.imageUrl} alt={product.name} />
+            <img src={product.imageUrl} alt={product.name} className="product-image" />
             <p>{product.description}</p>
-            <p>Price: ${product.price}</p>
+            <p>Price: ${product.price.toFixed(2)}</p>
             <p>Availability: {product.availability}</p>
             <button onClick={addToCart}>Add to Cart</button>
           </div>
